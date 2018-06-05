f87a9dac9456f0cb6744871fc0746cc26e5aefc6
sandeepreddyib <sandeepunix99@gmail.com>
2018-06-05 14:41:08 +0530

    vishnu added changes

--- file.java
+++ file.java
@@ -3,5 +3,8 @@ class file
    public static void main(String[] args)
    {
        System.out.println("Hello World");
+	  int a=10, b=20;
+	  int c=a+b;
+        System.out.println("c value = " + c);
    }
 }
