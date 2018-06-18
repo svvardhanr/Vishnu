35f3c48983dbb7f502f2b5acc001ccc21c15f34f
svvardhanr <39949194+svvardhanr@users.noreply.github.com>
2018-06-18 18:07:46 +0530

    Update file.java

--- file.java
+++ file.java
@@ -3,7 +3,7 @@ class file
    public static void main(String[] args)
    {
        System.out.println("Hello World");
-	  int a=10, b=20;
+	  int a=10, b=21;
 	  int c=a+b;
         System.out.println("c value = " + c);
 	System.out.println("Success");
